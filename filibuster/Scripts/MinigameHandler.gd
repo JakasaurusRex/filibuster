e279933e8ee143dee9f893b941db5466ec8da5c5
Birdboys <colbyjbird@comcast.net>
2026-06-22 21:07:45 -0600

    minigame shell is in

---
+++ filibuster/Scripts/MinigameHandler.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@onready var minigames := {
+	"test_2d_minigame" : {
+		"scene": preload("res://Assets/Scenes/Minigames/test2DMinigame/test_2d_minigame.tscn"),
+		"weight": 1
+	},
+	"fish_game": {
+		"scene": preload("res://Assets/Scenes/Minigames/FishMinigame/FishMinigame.tscn"),
+		"weight": 1
+	},
+}
+
+func get_random_minigame():
+	return minigames.keys().pick_random()
+
+func get_minigame_scene(minigame):
+	return minigames[minigame]['scene']
