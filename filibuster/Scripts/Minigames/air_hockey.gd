7466b8c4de84de0291ae1b42f6ecdae274dd133b
ColeVerc <vercauterencole@gmail.com>
2026-06-26 19:42:58 -0600

    Test

---
+++ filibuster/Scripts/Minigames/air_hockey.gd
@@ -0,0 +1,14 @@
+extends "res://Scripts/Minigames/FishMinigame/Camera.gd"
+
+func _ready() -> void:
+	pass
+	
+#func win():
+	#super.win()
+	#emit_signal("completed", "PLAYER WON AIR HOCKEY")
+	#close()
+#
+#func lose():
+	#super.lose()
+	#emit_signal("finished", "PLAYER LOST AIR HOCKEY")
+	#close()
